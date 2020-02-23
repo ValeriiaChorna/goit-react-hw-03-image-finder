67b73729b960fdc8f586b9716aaa6713057a7a27
Valeriia <valeria.chyornaya@gmail.com>
2020-02-23 17:48:21 +0200

    Updates

---
+++ precache-manifest.3c106c99b4b873c279881a2eaa2dc2a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc61856d06c09d9ece4a9437a7413c30",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "09176c8d6b98b5f9525e",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "d4243e38c0435cc934d6",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.5974bd81.chunk.css"
+  },
+  {
+    "revision": "09176c8d6b98b5f9525e",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.398d2e10.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.398d2e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4243e38c0435cc934d6",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.7e451c40.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  }
+]);
